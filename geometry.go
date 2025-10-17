03bb882821034db6e4b6572fb0bfc2566ec107bd
Morten Urban <morten.urban@mansio-logistics.com>
2025-10-17 12:21:04 +0200

    added support for geometry calculation

---
+++ geometry.go
@@ -0,0 +1,53 @@
+package navclient
+
+import (
+	"context"
+	"net/http"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/mansio-gmbh/goapiutils/ct"
+	"github.com/pkg/errors"
+)
+
+const (
+	geometryURL = "api/geometry"
+)
+
+type geometryRequest struct {
+	Routes                 LocationChains `json:"routes"`
+	SimplificationDistance float64        `json:"simplification_distance,omitempty"`
+}
+
+type geometryResponse struct {
+	Routes ChainResults `json:"routes"`
+}
+
+type GeometryResults map[string]struct {
+	TimeDistance ct.TimeDistance
+	Geometry     []ct.Coordinates
+}
+
+// Geometry calculates the time, distance and geometry for the given coordinate chains.
+// The coordinate chains are expected to be a slice of slices of coordinates.
+// The first coordinate of each chain is the start, the last the destination.
+func (c *Client) Geometry(ctx context.Context, coordinateChains LocationChains, distance float64) (GeometryResults, error) {
+	request := geometryRequest{
+		Routes:                 coordinateChains,
+		SimplificationDistance: distance,
+	}
+
+	res, err := c.doJSON(ctx, http.MethodPost, geometryURL, request)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var resp GeometryResults
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	decoder := json.NewDecoder(res.Body)
+	if err = decoder.Decode(&resp); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return resp, nil
+}
